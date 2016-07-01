cbf8f6be200cacc3280aa7713dc9a958e6f1a33a
WidgetsBurritos <davidstinemetze@gmail.com>
2016-07-01 08:51:45 -0500

    chore(release): 0.7.0

--- dist/js/zoolander.js
+++ dist/js/zoolander.js
@@ -1,4 +1,26 @@
 jQuery(document).ready(function() {
+  //main nav functionality
+  jQuery ("#search,#searchDesktop").click(function(){
+    $(".navbar-search").slideToggle();
+    $(".navbar-activeArrow").fadeToggle();
+    $(".navbar-menuContainer").removeClass("in");
+    $(".navbar-hamburger").addClass("collapsed");
+  });
+  jQuery (".navbar-searchInput").click(function(){
+    $(".navbar-searchButton").css('opacity', 1);
+  })
+  jQuery (".navbar-hamburger").click(function(){
+    $(".navbar-hamburger").toggleClass("collapsed");
+    $(".navbar-search").slideUp();
+    $(".navbar-activeArrow").fadeOut();
+  })
+  jQuery (".navbar-tertiary-dropDownTrigger").hover(function(e){
+    var current=$(this).find('.navbar-tertiary-dropDownMenu');
+    current.toggle();
+    e.stopPropagation();
+  });
+
+  //back to top
   var offset = 250;
   var duration = 300;
   jQuery ('.back-to-top').click (function (event) {
@@ -13,11 +35,9 @@ jQuery(document).ready(function() {
       jQuery ('.back-to-top').fadeOut (duration);
     }
   });
-});
 
-
-jQuery(document).ready(function ($) {
-    $('#myTab').tabCollapse();
+  //tab collapse
+  $('#myTab').tabCollapse();
     jQuery ('.title a').click(function (event) {
       jQuery (this).parent().parent().find('.fa-arrow-right').animate({  borderSpacing: +90 }, {
       step: function(now,fx) {
@@ -31,5 +51,4 @@ jQuery(document).ready(function ($) {
     });
 
     $('#footer-accordion').tabCollapse();
-
 });
