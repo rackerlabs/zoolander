dd7b373267de84e23b262c6e41f225df95b201c8
WidgetsBurritos <davidstinemetze@gmail.com>
2017-03-31 14:04:09 -0500

    chore(release): 0.17.4

--- dist/js/global.js
+++ dist/js/global.js
@@ -33,6 +33,121 @@ jQuery(document).ready(function ($) {
   };
 })(jQuery); // to protect and scope the JQuery alias = $
 
+// Filter bar plugin
+(function ($) {
+  $.fn.rsFilterBar = function rsFilterBar() {
+    // Variable declorations
+    var $filterBar = $(this);
+    var $filterForm = $filterBar.find('.rsFilter-form');
+    var $mainNavHeight = parseInt($('.navbar-fixed-top').outerHeight(), 10);
+    var inBounds = false;
+    var $stickyItemTop = void 0;
+
+    // Callback to get new form width on resizing/scrolling
+    function getWidth(bar, padding) {
+      var width = parseInt(bar.outerWidth(true), 10) - padding;
+      return width;
+    }
+
+    // Callback to check if inbounds of side menu
+    function checkInBounds(wt) {
+      var filterFormHeight = parseInt($filterForm.outerHeight(), 10);
+      if (!inBounds) {
+        $stickyItemTop = parseInt($filterForm.offset().top, 10);
+      }
+      var windowTopInBounds = $stickyItemTop - $mainNavHeight < wt;
+      var filterBottom = parseInt($filterBar.outerHeight(), 10) + parseInt($filterBar.offset().top, 10);
+      var formBottom = wt + filterFormHeight;
+      var filterGap = filterBottom + $mainNavHeight;
+      var filterInBounds = (filterGap - filterFormHeight) * 3 > formBottom;
+      inBounds = windowTopInBounds && filterInBounds;
+      return inBounds;
+    }
+
+    if ($filterForm.length) {
+      var $barPadding = parseInt($filterBar.css('padding-left'), 10) + parseInt($filterBar.css('padding-right'), 10);
+      var $filterHamburger = $filterBar.find('.rsFilter-hamburger');
+      var $rsWindow = $(window);
+      $stickyItemTop = parseInt($filterForm.offset().top, 10);
+      var resizeTimer = void 0;
+      var isFixed = false;
+
+      $filterHamburger.click(function () {
+        $filterForm.toggleClass('rsFilter-mobileForm');
+        setTimeout(function () {
+          $filterForm.toggleClass('rsFilter-mobileForm-effect');
+        }, 100);
+      });
+
+      $rsWindow.scroll(function () {
+        // If not in mobile
+        if ($rsWindow.outerWidth() > 767) {
+          var $rsWindowTop = parseInt($rsWindow.scrollTop(), 10);
+          inBounds = checkInBounds($rsWindowTop);
+          // If user scrolls to filter form and is in the bounds of the bar, move form up and down
+          if (inBounds) {
+            $filterForm.css({
+              position: 'fixed',
+              top: $mainNavHeight + 10,
+              width: getWidth($filterBar, $barPadding) + 'px'
+            });
+            isFixed = true;
+          } else {
+            $filterForm.css({
+              position: '',
+              width: ''
+            });
+            isFixed = false;
+          }
+        }
+      });
+
+      // Set a slight delay to the resize functionality so that it doesn't
+      // run on every pixel of a browser resize
+      $rsWindow.resize(function () {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(function () {
+          // reset this incase siblings above become taller on broweser resize
+          $stickyItemTop = parseInt($filterForm.offset().top, 10);
+          // If not mobile
+          if ($rsWindow.outerWidth() > 768) {
+            if (!isFixed && inBounds) {
+              $filterForm.css({
+                position: 'fixed',
+                width: getWidth($filterBar, $barPadding) + 'px'
+              });
+              isFixed = true;
+            } else if (isFixed && inBounds) {
+              $filterForm.css({
+                width: getWidth($filterBar, $barPadding) + 'px'
+              });
+            } else if (isFixed && !inBounds) {
+              $filterForm.css({
+                position: ''
+              });
+              isFixed = false;
+            }
+          } else {
+            // If in mobile
+            $filterForm.css({
+              width: ''
+            });
+            if (isFixed) {
+              $filterForm.css({
+                position: '',
+                width: ''
+              });
+              isFixed = false;
+            }
+          }
+        }, 250);
+      });
+    }
+
+    return this;
+  };
+})(jQuery);
+
 // swatches copy button
 (function () {
   function copy(e) {
@@ -78,7 +193,7 @@ jQuery(document).ready(function ($) {
 
   // Zoolander only solution for auto collapsing menu on example pages
   var url = window.location.pathname;
-  if (url.match(/\/derek\/incubation\//gi) && url !== '/derek/incubation/' || url.match(/\/derek\/solutions\//gi) && url !== '/derek/solutions/' || url.match(/\/derek\/templates\//gi) && url !== '/derek/templates/') {
+  if (url.match(/\/derek\/incubation\//gi) && url !== '/derek/incubation/' || url.match(/\/derek\/view-templates\//gi) && url !== '/derek/view-templates/' || url.match(/\/derek\/solutions\//gi) && url !== '/derek/solutions/' || url.match(/\/derek\/templates\//gi) && url !== '/derek/templates/') {
     zoolanderSlideBtn.trigger('click');
   }
 
---
+++ dist/js/view-templates/events/events.js
@@ -0,0 +1,5 @@
+'use strict';
+
+jQuery(document).ready(function ($) {
+  $('.rsFilter-bar').rsFilterBar();
+});
