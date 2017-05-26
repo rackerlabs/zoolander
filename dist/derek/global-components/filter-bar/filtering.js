a0bd76492878942f4ba84fae85b65aa7d3fdd532
WidgetsBurritos <davidstinemetze@gmail.com>
2017-05-26 13:22:24 -0500

    chore(release): 1.1.10

--- dist/derek/global-components/filter-bar/filtering.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(($) => {
-  $('.rsFilter-bar').rsFilterBar();
-});
--- dist/js/global-components/filter-bar/filtering.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-jQuery(document).ready(function ($) {
-  $('.rsFilter-bar').rsFilterBar();
-});
