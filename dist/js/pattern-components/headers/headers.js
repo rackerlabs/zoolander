df5fa7f3dc2a65bd277fa5e6ca41f6ffd3b439e1
WidgetsBurritos <davidstinemetze@gmail.com>
2017-10-11 09:21:47 -0500

    chore(release): 1.6.7

---
+++ dist/js/pattern-components/headers/headers.js
@@ -0,0 +1,15 @@
+
+
+// var resizeBannerLeft = () => {
+//
+// };
+//
+// jQuery(document).ready(($) => {
+//   console.log('dom ready');
+//   var $headline = $('.refresh-headline');
+//   $headline.on('fitTextLoaded', () => {
+//     const height = $headline.height();
+//     console.log(height);
+//   }).fitText(0.8, { minFontSize: '55px', maxFontSize: '55px' });
+// });
+"use strict";
