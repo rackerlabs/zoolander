089401b3b60e63844d7d7b40b7a4b19c584a9b31
WidgetsBurritos <davidstinemetze@gmail.com>
2017-10-17 13:26:28 -0500

    chore(release): 1.7.0

---
+++ dist/js/pattern-components/headers/headers.spec.js
@@ -0,0 +1,79 @@
+'use strict';
+
+describe('Test svgBannerGetAttributes()', function () {
+  it('has the correct single line response for primaryBox', function () {
+    var expectedResponse = {
+      boxHeight: 95,
+      leftSide: '777,95 140,777 0,777 777,0',
+      bottomSide: '1442,95 805,777 140,777 777,95'
+    };
+    expect(svgBannerGetAttributes(1, 'primaryBox')).to.eql(expectedResponse);
+  });
+
+  it('has the correct single line response for secondaryBox', function () {
+    var expectedResponse = {
+      boxHeight: 95,
+      leftSide: '777,95 140,777 0,777 777,0',
+      bottomSide: '1327,95 690,777 140,777 777,95'
+    };
+    expect(svgBannerGetAttributes(1, 'secondaryBox')).to.eql(expectedResponse);
+  });
+
+  it('has the correct double line response for primaryBox', function () {
+    var expectedResponse = {
+      boxHeight: 140,
+      leftSide: '777,140 140,777 0,777 777,0',
+      bottomSide: '1442,140 805,777 140,777 777,140'
+    };
+    expect(svgBannerGetAttributes(2, 'primaryBox')).to.eql(expectedResponse);
+  });
+
+  it('has the correct double line response for secondaryBox', function () {
+    var expectedResponse = {
+      boxHeight: 140,
+      leftSide: '777,140 140,777 0,777 777,0',
+      bottomSide: '1327,140 690,777 140,777 777,140'
+    };
+    expect(svgBannerGetAttributes(2, 'secondaryBox')).to.eql(expectedResponse);
+  });
+
+  it('has the correct triple line response for primaryBox', function () {
+    var expectedResponse = {
+      boxHeight: 185,
+      leftSide: '777,185 140,777 0,777 777,0',
+      bottomSide: '1442,185 805,777 140,777 777,185'
+    };
+    expect(svgBannerGetAttributes(3, 'primaryBox')).to.eql(expectedResponse);
+  });
+
+  it('has the correct triple line response for secondaryBox', function () {
+    var expectedResponse = {
+      boxHeight: 185,
+      leftSide: '777,185 140,777 0,777 777,0',
+      bottomSide: '1327,185 690,777 140,777 777,185'
+    };
+    expect(svgBannerGetAttributes(3, 'secondaryBox')).to.eql(expectedResponse);
+  });
+});
+
+describe('Test svgBannerStringSplitter()', function () {
+  it('does not split if string is short', function () {
+    var expectedResponse = ['abc def ghi'];
+    expect(svgBannerStringSplitter('abc def ghi', 15)).to.eql(expectedResponse);
+  });
+
+  it('does not split if string is exactly the right size', function () {
+    var expectedResponse = ['abc def ghi'];
+    expect(svgBannerStringSplitter('abc def ghi', 11)).to.eql(expectedResponse);
+  });
+
+  it('splits into two lines if string is long enough', function () {
+    var expectedResponse = ['abc def ghi', 'jkl mno'];
+    expect(svgBannerStringSplitter('abc def ghi jkl mno', 14)).to.eql(expectedResponse);
+  });
+
+  it('splits into three lines if string is long enough', function () {
+    var expectedResponse = ['abc def ghi', 'jkl mno pqr', 'stu vwx yz'];
+    expect(svgBannerStringSplitter('abc def ghi jkl mno pqr stu vwx yz', 14)).to.eql(expectedResponse);
+  });
+});
